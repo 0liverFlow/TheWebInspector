69463ad6c9758dfccd0314087e5bb0b79ab3c13d
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-05-05 06:06:50 -0700

    Add files via upload

---
+++ TheWebInspector.py
@@ -0,0 +1,249 @@
+import argparse
+import re
+import time
+import sys
+
+from web_inspect import WebInspect
+
+from rich import print as printc
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    parser = argparse.ArgumentParser(prog='main.py', description="Retrieve interesting information from the webpage's source code", epilog='Ping me: 0liverFlow@proton.me')
+    parser.add_argument('-v', '--verbose', action="count", help='increase output verbosity', default=0)
+    parser.add_argument('-u','--url', metavar="URL", help="target's url (e.g. https://example.com)", required=True)
+    args = parser.parse_args()
+    url = args.url
+
+    # Banner
+    print("""
+╔────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╗
+|                                                                                                                                    |
+| ████████╗██╗  ██╗███████╗    ██╗    ██╗███████╗██████╗     ██╗███╗   ██╗███████╗██████╗ ███████╗ ██████╗████████╗ ██████╗ ██████╗  | 
+| ╚══██╔══╝██║  ██║██╔════╝    ██║    ██║██╔════╝██╔══██╗    ██║████╗  ██║██╔════╝██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗ |
+|    ██║   ███████║█████╗      ██║ █╗ ██║█████╗  ██████╔╝    ██║██╔██╗ ██║███████╗██████╔╝█████╗  ██║        ██║   ██║   ██║██████╔╝ |
+|    ██║   ██╔══██║██╔══╝      ██║███╗██║██╔══╝  ██╔══██╗    ██║██║╚██╗██║╚════██║██╔═══╝ ██╔══╝  ██║        ██║   ██║   ██║██╔══██╗ |
+|    ██║   ██║  ██║███████╗    ╚███╔███╔╝███████╗██████╔╝    ██║██║ ╚████║███████║██║     ███████╗╚██████╗   ██║   ╚██████╔╝██║  ██║ |
+|    ╚═╝   ╚═╝  ╚═╝╚══════╝     ╚══╝╚══╝ ╚══════╝╚═════╝     ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝     ╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝ |
+|                                                    THE WEB INSPECTOR 1.0                                                           |
+|                                                 Coded with <3 by 0LIVERFLOW                                                        |
+╚────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╝                                  
+""")
+
+    ##############################
+    # Check if curl is installed
+    #############################
+    running_os = WebInspect.get_running_os()
+    if running_os:
+        is_curl_installed = WebInspect.is_installed('curl', running_os)
+        if not is_curl_installed:
+            sys.exit(printc("[red3][-][/red3] You need to install curl to run this script!!"))
+        elif is_curl_installed is None:
+            printc("[red3][-][/red3] Unknown Operating System!!")
+            printc("[gold1][!][/gold1] Supported OS are: GNU/Linux, Windows and MacOS")
+            sys.exit(printc("[gold1][!][/gold1] If you're sure that you are running one of these systems, thanks to report this issue at https://github.com/0liverFlow/TheWebInspector/issues"))
+    
+    inspected_webpage = WebInspect(url) # new instance of WebInspect class  
+
+    ###############
+    # Target URL
+    ##############
+    printc(f"\n[bright_blue][*][/bright_blue] Target URL: [red3]{inspected_webpage.url}[/red3]\n{'-'*45}")
+    ##############################
+    # Checking website language
+    ##############################
+    printc(f"\n[bright_blue][*][/bright_blue] Language\n{'-'*18}")
+    inspected_webpage.get_language()
+    if inspected_webpage.language != "N/A":
+        printc(f"[spring_green2][+][/spring_green2] {inspected_webpage.language}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.language}")
+
+    #########################
+    # Checking website title
+    #########################
+    inspected_webpage.get_title()
+    printc(f"\n[bright_blue][*][/bright_blue] Title\n{'-'*18}")
+    if inspected_webpage.title != "N/A":
+        printc(f"[spring_green2][+][/spring_green2] {inspected_webpage.title}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.title}")
+
+    #######################
+    # Allowed HTTP Methods
+    #######################
+    inspected_webpage.get_allowed_methods()
+    printc(f"\n[bright_blue][*][/bright_blue] Allowed Methods\n{'-'*19}")
+    if inspected_webpage.allowed_methods != "N/A":
+        printc(f"[spring_green2][+][/spring_green2] {inspected_webpage.allowed_methods}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.allowed_methods}")
+    
+    ########################            
+    # HTTP Response Headers
+    ########################
+    inspected_webpage.check_secured_http_response_headers()
+    printc(f"\n[bright_blue][*][/bright_blue] HTTP Response Headers\n{'-'*25}")
+    if len(inspected_webpage.unset_secured_http_response_headers):
+        for unset_secured_http_response_header in inspected_webpage.unset_secured_http_response_headers:
+            printc(f"[gold1][!][/gold1] {unset_secured_http_response_header} not found!")
+    else:
+        printc("[red3][-][/red3] N/A")
+    if len(inspected_webpage.juicy_headers):
+        for juicy_header, juicy_header_value in inspected_webpage.juicy_headers.items():
+            printc(f"[gold1][!][/gold1] {juicy_header}: {juicy_header_value}")
+    else:
+        printc("[red3][-][/red3] N/A")
+
+    #############
+    # Comments
+    #############
+    inspected_webpage.get_comments()
+    printc(f"\n[bright_blue][*][/bright_blue] Comments\n{'-'*18}")
+    if inspected_webpage.comments != 'N/A':
+        for comment_num, comment_tag in enumerate(inspected_webpage.comments, start=1):
+            if comment_tag:
+                printc(re.sub('\s\s+',' ', str(comment_tag).strip()))
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.comments}")
+
+    ###############
+    # Meta Tags
+    ###############
+    if args.verbose > 1:
+        inspected_webpage.get_meta_tags()
+        printc(f"\n[bright_blue][*][/bright_blue] Meta Tags\n{'-'*20}")
+        if inspected_webpage.meta_tags != 'N/A':
+            for meta_tag in inspected_webpage.meta_tags:
+                for meta_tag_attr in meta_tag.attrs:
+                    if 'name' in meta_tag_attr:
+                        printc(f"{meta_tag['name']}: {meta_tag['content']}")
+                    elif 'property' in meta_tag_attr:
+                        printc(f"{meta_tag['property']}: {meta_tag['content']}")
+        else:
+            printc(f"[red3][-][/red3] {inspected_webpage.meta_tags}")
+
+    #############
+    # Inputs
+    #############
+    inspected_webpage.get_inputs()
+    printc(f"\n[bright_blue][*][/bright_blue] Text Inputs\n{'-'*20}")
+    if inspected_webpage.text_inputs != "N/A":
+        for text_input in inspected_webpage.text_inputs:
+            printc(f"{str(text_input).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.text_inputs}")
+    printc(f"\n[bright_blue][*][/bright_blue] Password Inputs\n{'-'*19}")
+    if inspected_webpage.password_inputs != "N/A":
+        for password_input in inspected_webpage.password_inputs:
+            printc(f"{str(password_input).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.password_inputs}")
+    printc(f"\n[bright_blue][*][/bright_blue] File Inputs\n{'-'*18}")
+    if inspected_webpage.file_inputs != "N/A":
+        for file_input in inspected_webpage.file_inputs:
+            printc(f"{str(file_input).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.file_inputs}")
+    printc(f"\n[bright_blue][*][/bright_blue] Search Inputs\n{'-'*18}")
+    if inspected_webpage.search_inputs != "N/A":
+        for search_input in inspected_webpage.search_inputs:
+            printc(f"{str(search_input).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.search_inputs}")
+    printc(f"\n[bright_blue][*][/bright_blue] Email Inputs\n{'-'*18}")
+    if inspected_webpage.email_inputs != "N/A":
+        for email_input in inspected_webpage.email_inputs:
+            printc(f"{str(email_input).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.email_inputs}")
+    printc(f"\n[bright_blue][*][/bright_blue] Hidden Inputs\n{'-'*20}")
+    if inspected_webpage.hidden_inputs != "N/A":
+        for hidden_input in inspected_webpage.hidden_inputs:
+            printc(f"{str(hidden_input).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.hidden_inputs}")
+    
+    #####################
+    # Display: None Tags
+    #####################
+    if args.verbose > 1:
+        inspected_webpage.get_display_none_tags()
+        printc(f"\n[bright_blue][*][/bright_blue] Display None Tags\n{'-'*22}")
+        if inspected_webpage.display_none != "N/A":
+            for display_none in inspected_webpage.display_none:
+                printc(f"{display_none}")
+        else:
+            printc(f"[red3][-][/red3] {inspected_webpage.display_none}")
+
+    ##############
+    # Forms
+    ##############
+    if args.verbose > 1:
+        inspected_webpage.get_forms()
+        printc(f"\n[bright_blue][*][/bright_blue] Forms\n{'-'*20}")
+        if inspected_webpage.forms != "N/A":
+            for form_num, form_tag in enumerate(inspected_webpage.forms, start=1):
+                printc(f"[spring_green2][+][/spring_green2] Form n°{form_num}\n{'-'*12}")
+                printc(f"{form_tag}\n{'-'*12}")
+        else:
+            printc(f"[red3][-][/red3] {inspected_webpage.forms}")
+
+    ################            
+    # Robots.txt
+    ################
+    inspected_webpage.get_robots_txt()
+    printc(f"\n[bright_blue][*][/bright_blue] Robots.txt\n{'-'*20}")
+    if inspected_webpage.robots_txt not in ["N/A", "Robots.txt file empty!"]:
+        for robots_txt_rule in inspected_webpage.robots_txt:
+            if args.verbose == 2:
+                    printc(f"{robots_txt_rule.strip()}")
+            elif args.verbose == 1:
+                if not re.search("^(Disallow|User-agent|Allow)", robots_txt_rule.strip(), flags=re.IGNORECASE) is None:
+                    printc(f"{robots_txt_rule.strip()}")
+            else:   
+                if not re.search("^(Disallow|User-agent)", robots_txt_rule.strip(), flags=re.IGNORECASE) is None:
+                    printc(f"[spring_green2][+][/spring_green2] {robots_txt_rule.strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.robots_txt}")
+
+    ################            
+    # Sitemap.xml
+    ################
+    inspected_webpage.get_sitemap_xml()
+    printc(f"\n[bright_blue][*][/bright_blue] Sitemap.xml\n{'-'*20}")
+    if inspected_webpage.sitemap_xml not in ["N/A", "Sitemap file empty!"]:
+        printc(f"[spring_green2][+][/spring_green2] {inspected_webpage.url + '/sitemap.xml'} found!")
+        if args.verbose > 0:
+            for line in inspected_webpage.sitemap_xml:
+                printc(f"{str(line).strip()}")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.sitemap_xml}")
+
+    ##################
+    # Phpinfo
+    ##################
+    inspected_webpage.get_phpinfo()
+    printc(f"\n[bright_blue][*][/bright_blue] PHP Info\n{'-'*20}")
+    if inspected_webpage.phpinfo != "N/A":
+        printc(f"[spring_green2][+][/spring_green2] {inspected_webpage.phpinfo} found!")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.phpinfo}")
+
+    ##################
+    # Cgidir
+    ##################
+    inspected_webpage.get_cgidir()
+    printc(f"\n[bright_blue][*][/bright_blue] CGI Dir\n{'-'*20}")
+    if inspected_webpage.cgidir != "N/A":
+        print(f"[spring_green2][+][/spring_green2] {inspected_webpage.cgidir} found!")
+    else:
+        printc(f"[red3][-][/red3] {inspected_webpage.cgidir}")
+
+    ##################
+    # Elapsed time
+    ##################
+    end_time = time.perf_counter()
+    hours, minutes, seconds = inspected_webpage.determine_elapsed_time(start_time, end_time)
+    elapsed_time = inspected_webpage.format_time(hours, minutes, seconds)
+    printc(f"\n[bright_blue][*][/bright_blue] THE WEB INSPECTOR: [red3]{inspected_webpage.url}[/red3]'s source code inspected in [gold1]{elapsed_time}[/gold1]!")
---
+++ web_inspect.py
@@ -0,0 +1,230 @@
+import sys
+import random
+import subprocess
+import platform
+
+from rich import print as printc
+import requests
+from bs4 import BeautifulSoup as bsoup, Comment
+from bs4 import BeautifulSoup as bsoup, Comment
+
+class WebInspect:    
+    def __init__(self, url):
+        if url.startswith('http'):
+            self.url = url[:-1] if url.endswith('/')  else url
+            self.headers = {
+                "DNT": "1",
+                "Accept-encoding": "gzip, deflate, br",
+                "Referer": '/'.join(self.url.split('/')[:3]),
+                "Upgrade-Insecure-Requests": "1",
+                "User-agent": self.get_user_agent()
+            }
+            try:
+                self.response = subprocess.check_output(['curl','-A', self.get_user_agent(), '-L', '-s', self.url])
+                self.soup = bsoup(self.response, 'html.parser')
+            except subprocess.CalledProcessError:
+                sys.exit(printc("[red1 b][-][/red1 b] An error occured! Make sure you are connected to the Internet, then retry!"))
+        else:
+            sys.exit(printc("[red1 b][-][/red1 b] Incorrect URL format (ex: http[s]://example.com"))
+
+    @staticmethod
+    def get_running_os():
+        return platform.platform().split('-')[0]
+
+    @staticmethod   
+    def is_installed(program, running_os):
+        if "linux" in running_os.lower() or "macos" in running_os.lower():
+            try:
+                subprocess.check_output(["which", program])
+                return True
+            except subprocess.CalledProcessError:
+                return False
+        elif "windows" in running_os.lower():
+            try:
+                subprocess.check_output(["where", program])
+                return True
+            except subprocess.CalledProcessError:
+                return False
+        else:
+           return None
+
+    @staticmethod
+    def get_user_agent() -> str:
+        # Generate a random user-agent
+        with open('db/user_agents.db') as f:
+            user_agents = f.readlines()
+            return random.choice(user_agents)[:-1]
+
+    @staticmethod    
+    def determine_elapsed_time(start_time, end_time):
+        elapsed_time = end_time - start_time
+        hours = minutes = 0
+        seconds = elapsed_time
+        if elapsed_time >= 3600 :
+            hours = elapsed_time // 3600
+            elapsed_time = elapsed_time % 3600 #elapsed_time - (hours * 3600)
+        if elapsed_time >= 60 :
+            minutes = elapsed_time // 60
+            seconds = elapsed_time % 60
+        return hours, minutes, seconds
+
+    @staticmethod
+    def format_time(hours, minutes, seconds):
+        #Set hour.s format
+        time_format=str() if not hours else str(hours) + 'h:'
+        #Set minute format 
+        time_format += str(minutes) + 'min:' if minutes else ''        
+        return time_format + str(seconds)[:5] + 's'
+
+    def get_language(self):
+        # Returns the language used by the website
+        try:
+            webpage_language = self.soup.find('html')['lang']
+            self.language = webpage_language
+        except KeyError:
+            self.language = "N/A"
+
+    def get_title(self):
+        try:
+            webpage_title = self.soup.find('head').find('title').string
+            self.title = webpage_title
+        except AttributeError:
+            self.title = "N/A"
+    
+    def get_comments(self):
+        webpage_comments = self.soup.find_all(string=lambda text: isinstance(text, Comment))
+        if len(webpage_comments):
+            self.comments = webpage_comments
+        else:
+            self.comments = "N/A"
+    
+    def get_meta_tags(self):
+        webpage_meta_tags = self.soup.find('head').find_all("meta")
+        if len(webpage_meta_tags):
+            self.meta_tags = webpage_meta_tags
+        else:
+            self.meta_tags = "N/A"
+    
+    def get_inputs(self):
+        webpage_text_inputs = self.soup.find_all("input", type="text")
+        webpage_password_inputs = self.soup.find_all("input", type="password")
+        webpage_file_inputs = self.soup.find_all("input", type="file")
+        webpage_email_inputs = self.soup.find_all("input", type="email")
+        webpage_hidden_inputs = self.soup.find_all("input", type="hidden")
+        webpage_search_inputs = self.soup.find_all("input", type="search")
+        if len(webpage_text_inputs):
+            self.text_inputs = webpage_text_inputs
+        else:
+            self.text_inputs = "N/A"
+        if len(webpage_password_inputs):
+            self.password_inputs = webpage_password_inputs
+        else:
+            self.password_inputs = "N/A"
+        if len(webpage_file_inputs):
+            self.file_inputs = webpage_file_inputs
+        else:
+            self.file_inputs = "N/A"
+        if len(webpage_search_inputs):
+            self.search_inputs = list(set(webpage_search_inputs))
+        else:
+            self.search_inputs = "N/A"
+        if len(webpage_email_inputs):
+            self.email_inputs = webpage_email_inputs
+        else:
+            self.email_inputs = "N/A"
+        if len(webpage_hidden_inputs):
+            self.hidden_inputs = list(set(webpage_hidden_inputs))
+        else:
+            self.hidden_inputs = "N/A"
+    
+    def get_display_none_tags(self):
+        display_none_tags = self.soup.find_all(style=lambda style_attribute: style_attribute and 'display:none' in style_attribute)
+        if len(display_none_tags):
+            self.display_none = display_none_tags
+        else:
+            self.display_none = "N/A"
+
+    def get_forms(self):
+        forms = self.soup.find_all("form")
+        if len(forms):
+            self.forms = forms
+        else:
+            self.forms = "N/A"
+    
+    def get_robots_txt(self):
+        response_robots_txt = requests.get(self.url + '/robots.txt', headers=self.headers, allow_redirects=False)
+        if response_robots_txt.status_code == 200:
+            response_robots_txt_rules = response_robots_txt.text.split("\n")
+            if len(response_robots_txt_rules):
+                self.robots_txt = response_robots_txt_rules
+            elif response_robots_txt.ok:
+                self.robots_txt="Robots.txt file empty!"
+        else:
+            self.robots_txt = "N/A"
+
+    def get_sitemap_xml(self):
+        response_sitemap_xml = requests.get(self.url + '/sitemap.xml', headers=self.headers, allow_redirects=False)
+        if response_sitemap_xml.status_code == 200:
+            response_sitemap_xml_content = response_sitemap_xml.text.split()
+            if len(response_sitemap_xml_content):
+                self.sitemap_xml = response_sitemap_xml_content
+            else:
+                self.sitemap_xml="Sitemap file empty!"
+        else:
+            self.sitemap_xml = "N/A"
+
+    def check_secured_http_response_headers(self):
+        secured_http_response_headers = [
+            "Content-Security-Policy",
+            "X-XSS-Protection",
+            "X-Frame-Options",
+            "Strict-Transport-Security",
+            "X-Content-Type-Options",
+            "Referrer-Policy",
+            "Feature-Policy"
+        ]
+        self.unset_secured_http_response_headers = list()
+        response = requests.get(self.url, headers=self.headers)   
+        http_response_headers = response.headers
+        for secure_http_response_header in secured_http_response_headers:
+            if secure_http_response_header not in http_response_headers:
+                self.unset_secured_http_response_headers.append(secure_http_response_header)
+
+        juicy_headers = ["Server", "X-Powered-By"]
+        self.juicy_headers = dict()
+        for http_response_header in http_response_headers:
+            if http_response_header in juicy_headers:
+                self.juicy_headers[http_response_header] = http_response_headers[http_response_header]
+
+    def get_phpinfo(self):
+        # Checking phpinfo file
+        phpinfo_url = self.url + '/phpinfo.php'
+        phpinfo_response = requests.get(phpinfo_url, headers=self.headers, allow_redirects=False)
+        if phpinfo_response.status_code == 200:
+            self.phpinfo = phpinfo_response.url
+        else:
+            self.phpinfo = "N/A"
+           
+    def get_cgidir(self):
+        default_cgidirs = ['/admin/cgi-bin', '/cgi-bin/admin', '/cgi-bin']
+        for cgidir in default_cgidirs:
+            cgidir_url = self.url + cgidir
+            cgidir_response = requests.get(cgidir_url, headers=self.headers, allow_redirects=False)
+            if cgidir_response.status_code == 200:
+                self.cgidir = cgidir_response.url
+                break
+        else:
+            self.cgidir = "N/A"
+    
+    def get_allowed_methods(self):
+        allowed_methods = requests.options(self.url, headers=self.headers)
+        if allowed_methods.status_code == 200:
+            try:
+                if allowed_methods.headers["Allow"]:
+                    self.allowed_methods = allowed_methods.headers["Allow"]
+                else:
+                    self.allowed_methods = "N/A"
+            except KeyError:
+                self.allowed_methods = "N/A"
+        else:
+            self.allowed_methods = "N/A"
